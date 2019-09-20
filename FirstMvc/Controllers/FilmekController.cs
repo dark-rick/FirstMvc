60bbbc60688b6e5fefeeb0bc1e332c8568c0601c
Szűcs Richárd <szucsricsi12@gmail.com>
2019-09-20 19:13:53 +0200

    Add project files.

---
+++ FirstMvc/Controllers/FilmekController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvc.Models;
+
+namespace FirstMvc.Controllers
+{
+    public class FilmekController : Controller
+    {
+        // GET: Filmek
+        public ActionResult Random()
+        {
+
+
+            var film = new Film() { Cim = "Nincs ilyen" };
+            return View(film);
+        }
+    }
+}
